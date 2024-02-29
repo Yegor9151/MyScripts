f993202b4b985dca39b9d9e9d894507d6656e5b7
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2024-02-29 11:29:46 +0300

    new functions

---
+++ database_service.py
@@ -0,0 +1,46 @@
+﻿import time
+import pandas as pd
+import gspread, psycopg2
+
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+
+class GoogleService:
+
+    def __init__(self, token) -> None:
+        SCOPES = [
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/bigquery'
+        ]
+        cred = service_account.Credentials.from_service_account_info(token, scopes=SCOPES)
+        self.__gs_client = gspread.authorize(credentials=cred)
+        self.__bq_client = bigquery.Client(credentials=cred)
+
+    def collect_spreadsheets(self, key, title):
+        return self.__gs_client.open_by_key(key).worksheet(title).get_values()
+
+    def collect_data(self, query):
+        return self.__bq_client.query(query).to_dataframe()
+    
+
+class DWHService:
+
+    def __init__(self, token) -> None:
+        """DWH на PostgreSQL
+        Params:
+            :token: json - {
+                "host": str,
+                "database": str, 
+                "user": str,
+                "password": str
+            }
+        """
+        self.__token = token
+
+    def collect_data(self, query) -> pd.DataFrame:
+        with psycopg2.connect(**self.__token) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+        
+        return pd.DataFrame(cursor.fetchall(), columns=[col.name for col in cursor.description])
--- utils.py
+++ utils.py
@@ -1,40 +1,17 @@
 from os import listdir, mkdir
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from datetime import date, timedelta
 from requests import post
 
-
-class TempEditor:
-    
-    def __init__(self, temp):
-        """
-        Assemble query
-
-        params:
-            :temp: str = query template 
-        """
-        self.temp = temp
-        
-    def get_temp(self):
-        return self.temp
-
-    def replace(self, repl: dict):
-        """
-        Assemble query for DWH
-
-        params:
-            :repl: dict = {pattern_to_replace: new_value}
-        return: str =  prepeared query
-        """
-        query = self.temp
-        for tag, val in repl.items():
-            query = query.replace(tag, val)
-        return query
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
 
 
-def open_file(path, mode="r", encoding="utf-8", text=None):
+def open_file(path, mode="r", encoding=None, text=None):
     with open(path, mode=mode, encoding=encoding) as file:
-        if mode == "r":
+        if mode == "r" or mode == "rb":
             return file.read()
         elif mode == "w":
             file.write(text)
@@ -55,6 +32,23 @@ def create_dir(path: str) -> bool:
         return False
 
 
+def create_path(path):
+
+    dirs = path.split('/')
+    path = dirs[0]
+    for d in dirs[1:]:
+        path += f"/{d}"
+        create_dir(path)
+
+    return path
+
+
+def this_month():
+    date2 = date.today()
+    date1 = date(year=date2.year, month=date2.month, day=1)
+    return date1, date2
+
+
 def last_month():
     today = date.today()
     date2 = today - timedelta(days=today.day)
@@ -69,18 +63,33 @@ def half_month():
     return date1, date2
 
 
-def load_manager(path: str=None, func=None, low_memory=True, memory_map=False):
+def load_manager(path: str=None, func=None, low_memory=True, memory_map=False, parse_dates=False):
+    # Есть 3 сценария действий менеджера загрузки
     
+    # 1. Если путь не обозначен, тогда, просто выполняем функцию
     if not path:
         return func()
     
+    # 2. Разбиваем путь на части
     parts = path.split('/')
+    # Есть файл уже есть в папке, тогда читаем его
     if parts[-1] in listdir("/".join(parts[:-1])):
-        return read_csv(path, low_memory=low_memory, memory_map=memory_map)
+        return read_csv(path, low_memory=low_memory, memory_map=memory_map, parse_dates=parse_dates)
     
-    df = func()
-    df.to_csv(path, index=False)
-    return df
+    # 3. Выполняем функцию
+    data = func()
+    # Пробуем сохранить данные,
+    try:
+        # если они есть
+        if data.shape[0]:
+            data.to_csv(path, index=False)
+    # Если тип данных не верен,
+    except TypeError as err:
+        # тогда просто сохраняем файл
+        open_file(path, 'w', text=data)
+        print(err)
+    # возвращаем собранные данные
+    return data
 
 
 class TeleBot:
@@ -114,3 +123,136 @@ class TeleBot:
         post(URL, verify=False)
 
         return URL
+
+
+def parse_url(url):
+
+    root, params = url.split("?")
+    params = dict([p.split("=", 1) for p in params.split("&")])
+    
+    return root, params
+
+
+def reduce_memory(df: DataFrame):
+    
+    print("memory before:", round(df.memory_usage().sum() / (2**20), 4), "GB")
+
+    for col in df.columns:
+        
+        # Если это целыечисленные данные
+        if "int" in str(df[col].dtype):
+            
+            # если между данными нового и старого типа разницы нет, то меняем на новый
+            if not (df[col] != df[col].astype(f"int8")).sum():
+                df[col] = df[col].astype(f"int8")
+            elif not (df[col] != df[col].astype(f"int16")).sum():
+                df[col] = df[col].astype(f"int16")
+            elif not (df[col] != df[col].astype(f"int32")).sum():
+                df[col] = df[col].astype(f"int32")
+            elif not (df[col] != df[col].astype(f"int64")).sum():
+                df[col] = df[col].astype(f"int64")
+        
+        # Если это вещественные данные
+        elif "float" in str(df[col].dtype):
+            
+            # если между данными нового и старого типа разницы нет, то меняем на новый
+            if not (df[col] != df[col].astype(f"float16")).sum():
+                df[col] = df[col].astype(f"int16")
+            elif not (df[col] != df[col].astype(f"float32")).sum():
+                df[col] = df[col].astype(f"int32")
+            elif not (df[col] != df[col].astype(f"float64")).sum():
+                df[col] = df[col].astype(f"int64")
+                
+    print("memory after:", round(df.memory_usage().sum() / (2**20), 1), "GB")
+    
+    return df
+
+
+class Temp:
+    
+    def __init__(self, temp):
+        """
+        Assemble query
+
+        params:
+            :temp: str = query template 
+        """
+        self.temp = temp
+
+    def __repr__(self):
+        return self.temp
+
+    def replace(self, repl: dict):
+        """
+        Assemble query for DWH
+
+        params:
+            :repl: dict = {pattern_to_replace: new_value}
+        return: str
+        """
+        temp = self.temp
+        for tag, val in repl.items():
+            temp = temp.replace(tag, str(val))
+        return temp
+    
+    def add(self, lines: list[str], sep="\n"):
+        """
+        Add lines in end of text
+        
+        params:
+           :lines: list = [string1, string2, ...]
+        return: str
+        """
+        for l in lines:
+            if l not in self.temp:
+                self.temp += sep + l
+        return self.temp
+
+
+class Email:
+    
+    def __init__(self, host, port, user, password):
+        
+        self.__HOST = host
+        self.__PORT = port
+        self.__USER = user
+        self.__PASSWORD = password
+        
+        self.__msg = MIMEMultipart()
+        
+    def __repr__(self):
+        return f"HOST={self.__HOST}, PORT={self.__PORT}, USER={self.__USER}"
+        
+    def add_document(self, path):
+        
+        part = open_file(path, "rb")
+        part = MIMEApplication(part)
+        part['Content-Disposition'] = f"attachment; filename=\"{path.split('/')[-1]}\""
+
+        self.__msg.attach(part)
+        
+        return self.__msg
+        
+    def add_message(self, text):
+        
+        self.__msg.attach(MIMEText(text))
+        
+        return self.__msg
+        
+    def push(self, from_address:str, to_address:list, title=None):
+        
+        with smtplib.SMTP(self.__HOST, self.__PORT) as server:
+            server.starttls()
+            server.login(self.__USER, self.__PASSWORD)
+            
+            self.__msg["From"] = from_address
+            self.__msg["To"] = ", ".join(to_address)
+            self.__msg["Subject"] = title
+
+            server.send_message(self.__msg)
+
+            del self.__msg
+            
+            return True
+        
+        return False
