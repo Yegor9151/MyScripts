c36e57eedbe343fbdf67fcd84d3051b71a601f2d
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2025-06-30 13:32:09 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+import re
+import numpy as np
+import pandas as pd
 from os import listdir, mkdir
-from pandas import read_csv, DataFrame
-from datetime import date, timedelta
+import datetime
+# from datetime import datetime, date, timedelta
 from requests import post
 
-import smtplib
+import smtplib, json
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
@@ -15,6 +18,10 @@ def open_file(path, mode="r", encoding=None, text=None):
             return file.read()
         elif mode == "w":
             file.write(text)
+
+
+def read_json(path, encoding=None):
+    return json.loads(open_file(path, mode="r", encoding=encoding))
 
 
 def create_dir(path: str) -> bool:
@@ -34,47 +41,82 @@ def create_dir(path: str) -> bool:
 
 def create_path(path):
 
-    dirs = path.split('/')
-    path = dirs[0]
-    for d in dirs[1:]:
-        path += f"/{d}"
-        create_dir(path)
+    files = path.split('/') # разбиваем путь на файлы
+    if '.' in files[-1]: # если последний файл имеет расширение
+        files = files[:-1] # тогда исключаем этот файл из пути
+
+    path = files[0] # берем 1й файл
+    for d in files[1:]: # перебираем последующие в пути файлы
+        path += f"/{d}" # к предыдущему файлу в пути прибавляем следующий
+        create_dir(path) # достраиваем путь
 
     return path
 
 
-def this_month():
-    date2 = date.today()
-    date1 = date(year=date2.year, month=date2.month, day=1)
-    return date1, date2
+def this_week(dt=None, to_current=True):
+
+    de = dt if dt else datetime.date.today()
+
+    ds = de - datetime.timedelta(days=de.weekday())
+    
+    if not to_current:
+        de = ds + datetime.timedelta(days=6)
+
+    return ds, de
 
 
-def last_month():
-    today = date.today()
-    date2 = today - timedelta(days=today.day)
-    date1 = date2 - timedelta(days=date2.day - 1)
-    return date1, date2
+def this_month(dt=None, to_current=True):
+    de = dt if dt else datetime.date.today()
+    
+    if not to_current:
+        
+        next_month = de.month + 1
+        if next_month > 12:
+            de = de.replace(year=de.year+1, month=1, day=1) - datetime.timedelta(days=1)
+        else:
+            de = de.replace(month=next_month, day=1) - datetime.timedelta(days=1)
+
+    ds = de.replace(day=1)
+    return ds, de
+
+
+def prev_month(dt=None):
+    de = dt if dt else datetime.date.today()
+    de = de.replace(day=1) - datetime.timedelta(days=1)
+    ds = de - datetime.timedelta(days=de.day - 1)
+    return ds, de
 
 
 def half_month():
-    today = date.today()
-    date1 = date(year=today.year, month=today.month, day=1)
-    date2 = date(year=today.year, month=today.month, day=15)
+    today = datetime.date.today()
+    date1 = datetime.date(year=today.year, month=today.month, day=1)
+    date2 = datetime.date(year=today.year, month=today.month, day=15)
     return date1, date2
 
 
-def load_manager(path: str=None, func=None, low_memory=True, memory_map=False, parse_dates=False):
+def load_manager(path: str=None, func=None, low_memory:bool=True, memory_map:bool=False, parse_dates:list=False):
+    """
+    Need to ones preprocess data and save ones, then automaticaly upload precessed data
+    params:
+        path: str - path to save or upload data
+        func: - lambda function to processing data
 
-    # 1. 
+        https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+        low_memory: bool - internally process the file in chunks, if memory not enough to upload
+        memory_map: bool - can help to upload massive data
+        parse_datas: list - parse input columns to datetime type
+    """
+
+    # 1. if path not input, then use lambda function ewerytime
     if not path:
         return func()
     
-    # 2. 
+    # 2. if path input, and file exist then upload file
     parts = path.split('/')
     if parts[-1] in listdir("/".join(parts[:-1])):
-        return read_csv(path, low_memory=low_memory, memory_map=memory_map, parse_dates=parse_dates)
+        return pd.read_csv(path, low_memory=low_memory, memory_map=memory_map, parse_dates=parse_dates)
     
-    # 3.
+    # 3. if path input, but file not exist, then run lambda function, and save .csv result
     data = func()
     try:
         if data.shape[0]:
@@ -86,21 +128,20 @@ def load_manager(path: str=None, func=None, low_memory=True, memory_map=False, p
     return data
 
 
-class TeleBot:
+class TelegramBot:
 
-    def __init__(self, token, chat_id):
+    def __init__(self, token):
 
         self.__TOKEN = token
-        self.__CHAT_ID = chat_id
 
-    def send_documents(self, files_dir):
+    def send_documents(self, chat_id, files_dir):
         """push data in telegram iteratively
         
         Params:
             :files_dir - direction with files to upload in telegram chat
         Return: True if all is done"""
 
-        URL = f'https://api.telegram.org/bot{self.__TOKEN}/sendDocument?chat_id={self.__CHAT_ID}'
+        URL = f'https://api.telegram.org/bot{self.__TOKEN}/sendDocument?chat_id={chat_id}'
 
         for file in listdir(files_dir):
             if '.zip' in file:
@@ -110,11 +151,12 @@ class TeleBot:
 
         return URL
 
-    def send_message(self, text):
+    def send_message(self, chat_id, text):
 
-        URL = f'https://api.telegram.org/bot{self.__TOKEN}/sendMessage?chat_id={self.__CHAT_ID}&text={text}'
+        URL = f'https://api.telegram.org/bot{self.__TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
+        payload = {"parse_mode": "HTML"}
 
-        post(URL, verify=False)
+        post(URL, data=payload, verify=False)
 
         return URL
 
@@ -127,9 +169,9 @@ def parse_url(url):
     return root, params
 
 
-def reduce_memory(df: DataFrame):
+def reduce_memory(df: pd.DataFrame):
     
-    print("memory before:", round(df.memory_usage().sum() / (2**20), 4), "GB")
+    print("memory before:", round(df.memory_usage().sum() / (2**20), 4), "MB")
 
     for col in df.columns:
         
@@ -153,7 +195,7 @@ def reduce_memory(df: DataFrame):
             elif not (df[col] != df[col].astype(f"float64")).sum():
                 df[col] = df[col].astype(f"int64")
                 
-    print("memory after:", round(df.memory_usage().sum() / (2**20), 1), "GB")
+    print("memory after:", round(df.memory_usage().sum() / (2**20), 1), "MB")
     
     return df
 
@@ -244,3 +286,63 @@ class Email:
             del self.__msg
             
             return True
+
+
+class DFPreprocessor:
+
+    def __init__(self, df:pd.DataFrame):
+        self.__df = df.copy()
+
+    def __repr__(self):
+        return str(self.__df)
+
+    def get_df(self):
+        return self.__df
+
+    def drop_cols(self, columns:list):
+        
+        columns = self.__df.columns.isin(columns)
+        columns = self.__df.columns[columns]
+        self.__df = self.__df.drop(columns, axis=1)
+        
+        return self.__df
+
+    def execute_str(self, columns):
+
+        for col in columns:
+
+            # проверки
+            if type(self.__df.loc[0, col]) != str: # В этой колонке строки?
+                break
+
+            self.__df[col] = self.__df[col].apply(lambda x: re.sub(r"Decimal\d+[^\d]+|'\)", '', x))
+            self.__df[col] = self.__df[col].apply(lambda x: eval(x, {"datetime": datetime}))
+
+        return self.__df
+            
+
+    def explode(self, columns):
+
+        for col in columns:
+            self.__df = self.__df.explode(col).reset_index(drop=True)
+
+        return self.__df
+
+    def json_normalize(self, columns):
+
+        for col in columns:
+            self.__df = pd.concat([
+                self.__df, pd.json_normalize(self.__df[col])
+            ], axis=1)
+
+        return self.__df
+
+    def list_to_onehot(self, columns:list):
+
+        for col in columns:
+            for values in np.unique(self.__df[col]):
+                rows = self.__df[col].apply(lambda x: x == values)
+                self.__df.loc[rows, values] = 1
+                self.__df[values] = self.__df[values].fillna(0)
+
+        return self.__df
